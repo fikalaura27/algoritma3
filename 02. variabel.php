aa543c64f8f33d66344101f273fb938fc577aefe
fikalaura27 <fkalractracitra@gmail.com>
2024-08-29 10:51:06 +0700

    print r return value.php

---
+++ 02. variabel.php
@@ -0,0 +1,20 @@
+<?php
+
+$nama = 'fika';
+echo "Hallo, $nama!<br><br>";// Hasil: Hallo, fika!
+
+//menampilkan variabel 
+$umur = 16;
+echo "Umur saya adalah $umur tahun.<br><br>";
+
+//mengubah Teks dan variabel 
+$depan ='fika';
+$belakang = 'laura';
+echo "Nama saya adalah " . $depan . " ". $belakang . ".<br><br>";
+
+//menggunakan Echo untuk HTML 
+echo "<h1>judul halaman</h1>";
+echo "<p>paragraf ini di buat dengan PHP .</p>";
+
+echo "<strong>by : fika laura";
+
---
+++ d03. menampil kan return value dan prin_r.php
@@ -0,0 +1,9 @@
+<?php
+
+$buah = array("nanas", "anggur", "semangka");
+$hasil1 = print_r($buah, true);
+$hasil2 = print_r($buah, false);
+
+echo $hasil1;
+print "<p>";
+echo $hasil2;
