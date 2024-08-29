25fcdbf0159422d22521f45d1bd6afada73eb89b
fikalaura27 <fkalractracitra@gmail.com>
2024-08-29 11:26:22 +0700

    vardump pre.php

---
+++ 06.vardump object.php
@@ -0,0 +1,13 @@
+<?php
+class Mobil {
+    public $merk;
+    public $model;
+}
+
+$mobil_baru = new Mobil();
+$mobil_baru->merk = "pajero";
+$mobil_baru->model = "Corolla";
+
+var_dump($mobil_baru);
+print_r("<p><strong>by :fika laura");
+?>
---
+++ 07. vardump pre.php
@@ -0,0 +1,6 @@
+<?php
+$variabel_array = array("apel", "pisang", "ceri");
+echo "<pre>";
+var_dump($variabel_array);
+echo "</pre>";
+?>
