cc2dd7aca95c680afcdbce0c7f4f482428179aff
fikalaura27 <fkalractracitra@gmail.com>
2024-08-29 12:03:36 +0700

    print r aray.php

---
+++ 11. print r array php.php
@@ -0,0 +1,20 @@
+<?php
+$array = array(
+    'name' => 'John Doe',
+    'age' => 30,
+    'skills' => array('PHP', 'JavaScript', 'HTML')
+);
+
+print_r($array);
+
+print "<p>";
+
+$array = array(
+    'name' => 'John Doe',
+    'age' => 30,
+    'skills' => array('PHP', 'JavaScript', 'HTML')
+);
+
+print_r($array);
+
+print "<p>";
