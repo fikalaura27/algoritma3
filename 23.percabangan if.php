0fa45c424c747cd763149073b4c63e905052470b
fikalaura27 <fkalractracitra@gmail.com>
2024-08-30 10:15:16 +0700

    23.percabangan if.php

---
+++ 23.percabangan if.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$total_belanja = 150000;
+
+if($total_belanja > 100000){
+    echo "Anda dapat hadiah karena belanja anda melebihi $total_belanja , maka anda akan dapat diskon 75%!";
+}
+
+$siswa = "rajin";
+if($siswa = "rajin"){
+    echo "<hr>jika rajin akan NAIK KELAS!";
+}
