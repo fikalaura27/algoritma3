5734ede7bb989d3f1a044e2f0c94450f5c249001
fikalaura27 <fkalractracitra@gmail.com>
2024-08-30 10:44:30 +0700

    27. percabangan dengan operator rernary.php

---
+++ 27.percabangan dengan operator ternry.php
@@ -0,0 +1,7 @@
+<?php
+
+$suka = false;
+$suka ? print "saya suka kamu<hr>": print "mohon maaf saya uda ada yang punya!";
+
+$suka = true;
+echo $suka ? "Saya suka kamu<hr>": "Mohon Maaf saya udah ada yang punya! <hr>";
