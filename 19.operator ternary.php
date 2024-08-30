6a3ef385c45f685c72765b7740a3b9ad3b0529d2
fikalaura27 <fkalractracitra@gmail.com>
2024-08-30 09:44:30 +0700

    19.operator ternary.php

---
+++ 19.operator ternary.php
@@ -0,0 +1,17 @@
+<?php
+
+$suka = true;
+
+// menggunakan operator ternary
+$jawab = $suka ? "iya": "tidak";
+
+// menampilkan jawaban
+echo $jawab;
+echo "<br>";
+var_dump ($iya);
+echo "<hr>";
+
+echo $suka;
+echo "<br>";
+var_dump ($tidak);
+echo "<hr>";
