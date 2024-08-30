39b151a45a47575474b5c95b8dd7caf9d7c00b9d
fikalaura27 <fkalractracitra@gmail.com>
2024-08-30 11:10:22 +0700

    32.pengguna eleseif.php

---
+++ 32.pengguna elseif.php
@@ -0,0 +1,14 @@
+<?php
+// Misalnya, kamu ingin menampilkan pesan yang berbeda berdasarkan usia pengguna:
+$usia = 20;
+
+if ($usia < 13) {
+    echo "Kamu masih anak-anak.";
+} elseif ($usia >= 13 && $usia <= 19) {
+    echo "Kamu anak muda.";
+} elseif ($usia > 19 && $usia <= 30) {
+    echo "Kamu adalah sudah tumbuh menjadi dewasa.";
+} else {
+    echo "Kamu adalah dewasa.";
+}
+echo "<p><strong>by : fika laura";
