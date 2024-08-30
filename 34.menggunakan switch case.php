33534f3e8c46cd23297073fbeae7a343bae99f9e
fikalaura27 <fkalractracitra@gmail.com>
2024-08-30 11:20:23 +0700

    34.menggunakan switch case.php

---
+++ 34.menggunakan switch case.php
@@ -0,0 +1,14 @@
+<?php
+
+switch ($variable) {
+    case 'value1':
+        // kode yang akan dijalankan jika $variable == 'value1'
+        break;
+    case 'value2':
+        // kode yang akan dijalankan jika $variable == 'value2'
+        break;
+    // Tambahkan lebih banyak case sesuai kebutuhan
+    default:
+        // kode yang akan dijalankan jika tidak ada case yang cocok
+}
+echo "<p><strong>by : fika laura";
